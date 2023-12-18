3d54e3a6154a0ac8d549bc6191593f5014204ab9
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-18 14:52:23 -0500

    add initial logic to hangman game

--- Hangman Game/hangman-game.py
+++ Hangman Game/hangman-game.py
@@ -4,6 +4,20 @@ def play():
     print("Welcome to Hangman Game v{}!".format(version))
     print("*********************************")
 
+    secret_word = "banana"
+    hanged = False
+    got_it = False
+
+    while (not got_it and not hanged):
+        guess = input("Choose a letter: ")
+
+        index = 0
+        for letter in secret_word:
+            if (guess == letter):
+                print(f"I found the letter {letter} at position {index}")
+            index = index + 1
+
+    print("End")
 
 # verifying if python is running or is running as a module
 # we can create a hub of games so we can call this game as a module
