229b31cbfa220d2658cb2524215519c33f6745dd
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-18 12:19:01 -0500

    adding while loop

--- Gessing Game/gessing-game.py
+++ Gessing Game/gessing-game.py
@@ -4,23 +4,30 @@ print("Welcome to Gessing Game v{}!".format(version))
 print("*********************************")
 
 secret_number = 42
+total_attempts = 3
+attempt = 1
 
-input_str = input("Enter your number: ")
-print("Your number is: {}".format(input_str))
-# input returns a STRING, we need convert it to integer and compare to 
-# SECRET_NUMBER bellow 
-input_number = int(input_str)
+while (attempt <= total_attempts):
+    print("Attempt", attempt, "of", total_attempts)
+    input_str = input("Enter your number: ")
+    print("Your number is: {}".format(input_str))
+    # input returns a STRING, we need convert it to integer and compare to
+    # SECRET_NUMBER bellow
+    input_number = int(input_str)
 
-correct = secret_number == input_number
-greater = input_number > secret_number
-less = input_number < secret_number
+    correct = secret_number == input_number
+    greater = input_number > secret_number
+    less = input_number < secret_number
 
-if (correct):
-    print("You got it!")
-else:
-    if(greater):
-        print("You failed! Your number is greater than the secret number")
-    elif(less):
-        print("You failed! Your number is less than the secret number")
+    if (correct):
+        print("You got it!")
+        break # exit from loop while
+    else:
+        if (greater):
+            print("You failed! Your number is greater than the secret number")
+        elif (less):
+            print("You failed! Your number is less than the secret number")
+
+    attempt = attempt + 1
 
 print("End")
