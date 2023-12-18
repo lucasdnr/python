570b0f89038e4d6bab92925e1ba2e817a8c69b0c
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-18 16:22:33 -0500

    add error limit to attempts

--- Hangman Game/hangman-game.py
+++ Hangman Game/hangman-game.py
@@ -4,26 +4,36 @@ def play():
     print("Welcome to Hangman Game v{}!".format(version))
     print("*********************************")
 
-    secret_word = "banana"
+    secret_word = "banana".upper()
     hanged = False
     got_it = False
+    error = 0
     found_letters = ["_", "_", "_", "_", "_", "_"]
+
+    # first print to show to user how many letters the secret_word have
+    print(found_letters)
 
     while (not got_it and not hanged):
         guess = input("Choose a letter: ")
         # removing spaces from input
-        guess = guess.strip()
+        guess = guess.strip().upper()
 
-        index = 0
-        # searching for character in the string
-        for letter in secret_word:
-            if (guess.upper() == letter.upper()):
-                found_letters[index] = letter
-                # print(f"I found the letter {letter} at position {index}")
-            index = index + 1
+        if (guess in secret_word):
+            index = 0
+            # searching for character in the string
+            for letter in secret_word:
+                if (guess == letter):
+                    found_letters[index] = letter
+                    # print(f"I found the letter {letter} at position {index}")
+                index += 1
+        else:
+            error += 1
+
+        hanged = error == len(secret_word)
         print(found_letters)
 
     print("End")
+
 
 # verifying if python is running or is running as a module
 # we can create a hub of games so we can call this game as a module
