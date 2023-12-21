1c71a815ea119b7de012768b863cca7d4f717604
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-21 00:43:22 -0500

    add options and refactor functions

--- Delivery App/delivery-app.py
+++ Delivery App/delivery-app.py
@@ -1,29 +1,61 @@
 
-def print_welcome_message():
-    version = "0.1.0"
-    print("*********************************")
-    print(f"Delivery App v{version}")
-    print("*********************************")
+import os
 
-def print_menu()
-    print("1. New Restaurant")
-    print("2. List Restaurants")
-    print("3. Activate Restaurant")
-    print("4. Exit")
+
+def print_welcome_message():
+    version = '0.1.0'
+    print('*********************************')
+    print(f'v{version}')
+    print("""
+██████╗░███████╗██╗░░░░░██╗██╗░░░██╗███████╗██████╗░██╗░░░██╗  ░█████╗░██████╗░██████╗░
+██╔══██╗██╔════╝██║░░░░░██║██║░░░██║██╔════╝██╔══██╗╚██╗░██╔╝  ██╔══██╗██╔══██╗██╔══██╗
+██║░░██║█████╗░░██║░░░░░██║╚██╗░██╔╝█████╗░░██████╔╝░╚████╔╝░  ███████║██████╔╝██████╔╝
+██║░░██║██╔══╝░░██║░░░░░██║░╚████╔╝░██╔══╝░░██╔══██╗░░╚██╔╝░░  ██╔══██║██╔═══╝░██╔═══╝░
+██████╔╝███████╗███████╗██║░░╚██╔╝░░███████╗██║░░██║░░░██║░░░  ██║░░██║██║░░░░░██║░░░░░
+╚═════╝░╚══════╝╚══════╝╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░""")
+    print('*********************************')
+
+
+def print_menu():
+    print('1. New Restaurant')
+    print('2. List Restaurants')
+    print('3. Activate Restaurant')
+    print('4. Exit')
+
+
+def exit_app():
+    # clear console
+    os.system('cls')
+    # s.system('clear') for mac
+    print('Exit')
+
+
+def choose_option():
+    # select an option
+    option_selected = int(input('Choose an option: '))
+    match option_selected:
+        case 1:
+            print('New Restaurant')
+        case 2:
+            print('List Restaurants')
+        case 3:
+            print('Activate Restaurant')
+        case _:
+            exit_app()
+
 
 def run():
     # welcome message
     print_welcome_message()
-    
-    # print menu with options
-    print_menu
 
-    # select an option
-    option_selected = input('Choose an option: ')
-    print(f'You choose the option {option_selected}')
+    # print menu with options
+    print_menu()
+
+    # receive the input option from user and run the appropriate function of option
+    choose_option()
 
 
 # verifying if python is running or is running as a module
 # we can create a hub of applications so we can call this app as a module
-if (__name__ == "__main__"):
+if (__name__ == '__main__'):
     run()
