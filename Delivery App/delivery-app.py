a231a2f29c5437d9a9beb217c765dffa0e88126e
Lucas Ribeiro <lucas.daniel@gmail.com>
2023-12-21 09:48:25 -0500

    handling exceptions

--- Delivery App/delivery-app.py
+++ Delivery App/delivery-app.py
@@ -30,21 +30,39 @@ def exit_app():
     print('Exit')
 
 
+def invalid_option():
+    print('Invalid Option!\n')
+    input('Press any key to return to main menu')
+    # clear console
+    os.system('cls')
+    main()
+
+
 def choose_option():
-    # select an option
-    option_selected = int(input('Choose an option: '))
-    match option_selected:
-        case 1:
-            print('New Restaurant')
-        case 2:
-            print('List Restaurants')
-        case 3:
-            print('Activate Restaurant')
-        case _:
-            exit_app()
+    try:
+        # select an option
+        option_selected = int(input('Choose an option: '))
+        match option_selected:
+            case 1:
+                # add new restaurant
+                print('New Restaurant')
+            case 2:
+                # return a list of restaurants
+                print('List Restaurants')
+            case 3:
+                # activate a restaurant
+                print('Activate Restaurant')
+            case 4:
+                # exit app
+                exit_app()
+            case _:
+                # invalid option
+                invalid_option()
+    except:
+        invalid_option()
 
 
-def run():
+def main():
     # welcome message
     print_welcome_message()
 
@@ -58,4 +76,4 @@ def run():
 # verifying if python is running or is running as a module
 # we can create a hub of applications so we can call this app as a module
 if (__name__ == '__main__'):
-    run()
+    main()
