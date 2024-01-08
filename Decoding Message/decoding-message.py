c677d723757a50c1442069de3d2503ba05755f40
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-08 13:35:21 -0500

    code decoding message

--- Decoding Message/decoding-message.py
+++ Decoding Message/decoding-message.py
@@ -1,27 +1,51 @@
+def create_staircase(nums):
+    step = 1
+    subsets = []
+    while len(nums) != 0:
+        if len(nums) >= step:
+            subsets.append(nums[0:step])
+            nums = nums[step:]
+            step += 1
+        else:
+            return False
+
+    return subsets
+
 def decode(message_file):
     # Read the content of the file
     with open(message_file, 'r') as file:
         lines = file.readlines()
 
-    # Extract numbers and words from each line
-    pyramid = [line.strip().split()[1] for line in lines]
+    # Create a dictionary of numbers and words
+    num_word_dict = {}
+    for line in lines:
+        num, word = line.split()
+        num_word_dict[int(num)] = word
 
-    # Create a dictionary to store the words with their corresponding numbers
-    words_dict = {}
-    current_number = 1
+    # Create a list of numbers in ascending order
+    nums = sorted(list(num_word_dict.keys()))
 
-    for word in pyramid:
-        words_dict[current_number] = word
-        current_number += 1
+    # Sort the numbers to create the staircase
+    sorted_numbers = sorted(nums)
 
-    # Get the decoded message by joining the words in the correct order
-    decoded_message = ' '.join(words_dict.values())
+    # Create the staircase
+    staircase = create_staircase(sorted_numbers)
+
+    if not staircase:
+        return "Invalid input file format"
+
+    # Extract the words corresponding to the staircase numbers
+    # decoded_words = [words_and_numbers[line[-1] - 1][1] for line in staircase]
+    decoded_words = [num_word_dict[line[-1]] for line in staircase]
+
+    # Join the words to form the decoded message
+    decoded_message = ' '.join(decoded_words)
 
     return decoded_message
 
-
 # verifying if python is running or is running as a module
 if (__name__ == "__main__"):
-    message_file = "example.txt"  # Replace with the actual file name
+    # Replace with the actual file name
+    message_file = "./Decoding Message/example.txt"
     decoded_message = decode(message_file)
     print(decoded_message)
