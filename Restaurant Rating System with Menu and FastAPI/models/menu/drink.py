659e3cb567ee045d70888075e2a548386586bf3d
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-11 15:45:36 -0500

    Inheritance classes

--- Restaurant Rating System with Menu and FastAPI/models/menu/drink.py
+++ Restaurant Rating System with Menu and FastAPI/models/menu/drink.py
@@ -1,3 +1,9 @@
+from models.menu.item_menu import ItemMenu
+
+# specifying that the Drink class will inherit from the ItemMenu class
+
+
 class Drink:
-    def __init__(self, name, price, description):
-        pass
+    def __init__(self, name, price, size):
+        super().__init__(name, price)
+        self.size = size
--- Restaurant Rating System with Menu and FastAPI/models/menu/menu_dish.py
+++ Restaurant Rating System with Menu and FastAPI/models/menu/menu_dish.py
@@ -1,3 +1,9 @@
-class MenuDish:
+from models.menu.item_menu import ItemMenu
+
+# specifying that the MenuDish class will inherit from the ItemMenu class
+
+
+class MenuDish(ItemMenu):
     def __init__(self, name, price, description):
-        pass
+        super().__init__(name, price)
+        self.description = description
