7633f576bd6e0db46dcefd3e96ef721e34fde9b8
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-13 23:09:12 -0500

    add route to create new game

--- Flask - GameLib/game-lib.py
+++ Flask - GameLib/game-lib.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models.game import Game
 
 app = Flask(__name__)
 
 # routes
 
+
+def populate_list():
+    game_a = Game('Tetris', 'Puzzle', 'Atari')
+    game_b = Game('God of War', 'Rack n Slash', 'PS')
+    game_c = Game('Valorant', 'Shooter', 'PC')
+    return [game_a, game_b, game_c]
 
 @app.route('/hello')
 def hello():
@@ -14,25 +20,39 @@ def hello():
     return '<h1>Hello World!</h1>'
 
 
-@app.route('/list')
-def list():
+@app.route('/')
+def index():
     '''
     Return a List of Games
     '''
-    game_a = Game('Tetris', 'Puzzle', 'Atari')
-    game_b = Game('God of War', 'Rack n Slash', 'PS')
-    game_c = Game('Valorant', 'Shooter', 'PC')
-    list_games = [game_a, game_b, game_c]
+    list_games = populate_list()
     return render_template('list.html', title='Games', games=list_games)
 
 
 @app.route('/new')
 def new():
     '''
-    Add new game
+    Render the new game page
     '''
-
     return render_template('new.html', title='Games')
 
 
-app.run()
+@app.route('/create', methods=['POST'])
+def create():
+    '''
+    Create new game
+    '''
+
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+
+    list_games = populate_list()
+
+    game = Game(name, category, console)
+    list_games.append(game)
+
+    return render_template('list.html', title='Games', games=list_games)
+
+
+app.run(debug=True)
